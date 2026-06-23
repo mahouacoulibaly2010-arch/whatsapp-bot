e64ab17b4a2a1cf652faa5632ae316a04dc9e935
Mahoua Coulibaly <mahouacoulibaly@Mahouas-MacBook-Air.local>
2026-06-23 16:41:20 +0100

    Fix Railway port

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+import os
 
 app = Flask(__name__)
 
@@ -20,6 +22,36 @@ def webhook():
         message.body("Je suis MCEA IA Mentor. Comment puis-je vous aider ?")
 
     return str(response)
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8080))
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
